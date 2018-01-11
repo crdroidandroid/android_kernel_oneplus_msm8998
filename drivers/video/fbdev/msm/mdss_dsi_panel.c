ce5f99b11ea7e9d784e21e30084d9f7b09783aa4
Nathan Chancellor <natechancellor@gmail.com>
2018-01-11 16:58:53 -0700

    msm: mdss: Don't use OnePlus's hacky backlight remap on custom ROMs
    After the commit to chang the default max brightness value for the
    OnePlus 5T's panel, this is being used, causing the panel to get
    very dim. Only use this on OOS.
    
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>

--- drivers/video/fbdev/msm/mdss_dsi_panel.c
+++ drivers/video/fbdev/msm/mdss_dsi_panel.c
@@ -1026,6 +1026,7 @@ backlight level 0-->55 ==> 0-->55
 backlight level 55-->230 ==> 55-->200
 backlight level 230-->255 ==> 200-->255
  *************************************/
+#ifndef CONFIG_CUSTOM_ROM
 static u32 backlight_level_remap(struct mdss_dsi_ctrl_pdata *ctrl, u32 level)
 {
 	u32 remap_level = 0;
@@ -1043,6 +1044,7 @@ static u32 backlight_level_remap(struct mdss_dsi_ctrl_pdata *ctrl, u32 level)
 	}
 	return remap_level;
 }
+#endif
 
 static void mdss_dsi_panel_bl_ctrl(struct mdss_panel_data *pdata,
 							u32 bl_level)
@@ -1063,10 +1065,12 @@ static void mdss_dsi_panel_bl_ctrl(struct mdss_panel_data *pdata,
 	ctrl_pdata = container_of(pdata, struct mdss_dsi_ctrl_pdata,
 				panel_data);
 
+#ifndef CONFIG_CUSTOM_ROM
 	if (ctrl_pdata->high_brightness_panel) {
 	  pr_debug("%s goto backlight level remap\n", __func__);
 	  bl_level = backlight_level_remap(ctrl_pdata, bl_level);
 	}
+#endif
 	/*
 	 * Some backlight controllers specify a minimum duty cycle
 	 * for the backlight brightness. If the brightness is less
