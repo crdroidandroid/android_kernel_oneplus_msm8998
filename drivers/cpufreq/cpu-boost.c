4f8b347f5d6a3739b4da20e59135f5b8fd40019c
Pranav Vashi <neobuddy89@gmail.com>
2018-02-26 23:33:40 +0530

    cpufreq: cpu-boost: Prevent boost when device device is suspended
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- drivers/cpufreq/cpu-boost.c
+++ drivers/cpufreq/cpu-boost.c
@@ -22,6 +22,9 @@
 #include <linux/slab.h>
 #include <linux/input.h>
 #include <linux/time.h>
+#ifdef CONFIG_STATE_NOTIFIER
+#include <linux/state_notifier.h>
+#endif
 
 struct cpu_sync {
 	int cpu;
@@ -230,6 +233,11 @@ static void cpuboost_input_event(struct input_handle *handle,
 		unsigned int type, unsigned int code, int value)
 {
 	u64 now;
+
+#ifdef CONFIG_STATE_NOTIFIER
+	if (state_suspended)
+		return;
+#endif
 
 	if (!input_boost_enabled)
 		return;
