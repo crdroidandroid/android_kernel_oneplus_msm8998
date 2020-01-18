7c174b261b6b07150c298f7aad6f38144fe073cc
Pranav Vashi <neobuddy89@gmail.com>
2020-01-18 22:23:38 +0530

    Update wifi driver stack from LA.UM.7.2.r2-04200-8x98.0
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- drivers/staging/qcacld-3.0/core/mac/inc/qwlan_version.h
+++ drivers/staging/qcacld-3.0/core/mac/inc/qwlan_version.h
@@ -32,9 +32,9 @@
 #define QWLAN_VERSION_MAJOR            5
 #define QWLAN_VERSION_MINOR            1
 #define QWLAN_VERSION_PATCH            1
-#define QWLAN_VERSION_EXTRA            "R"
-#define QWLAN_VERSION_BUILD            73
+#define QWLAN_VERSION_EXTRA            "M"
+#define QWLAN_VERSION_BUILD            75
 
-#define QWLAN_VERSIONSTR               "5.1.1.73R"
+#define QWLAN_VERSIONSTR               "5.1.1.75M"
 
 #endif /* QWLAN_VERSION_H */
